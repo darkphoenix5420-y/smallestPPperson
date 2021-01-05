fc484abdd842f210e220e4140ad3766e5d99f413
Belva J Hunter <68235677+darkphoenix5420-y@users.noreply.github.com>
2021-01-05 23:20:35 +0530

    Create smallestPPperson.cpp
    Added something for Samuel

---
+++ smallestPPperson.cpp
@@ -0,0 +1,16 @@
+// Please take my name in video as Largest_PP_person
+// Please its a request
+// And this code is just for roast. I love your videos really 🤩
+
+#include<iostream>
+using namespace std;
+
+#define You int
+#define are main(){
+#define the printf(
+#define smallest "Hello World"
+#define PP );
+#define person return 0;
+#define in_the_world }
+
+You are the smallest PP person in_the_world
